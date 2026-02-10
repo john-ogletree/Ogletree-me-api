8e470cecbe3db3840219858183c6093871ac2b39
john-ogletree <pxltr3@gmail.com>
2026-02-10 10:46:28 -0500

    Refactor: Make API server independent of Echo-site static serving

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Serve static files from the 'Echo-site' directory
-const echoSitePath = path.join(__dirname, '..', 'Echo-site');
-app.use(express.static(echoSitePath));
-
-// Route for the homepage
-app.get('/', (req, res) => {
-  res.sendFile(path.join(echoSitePath, 'index.html'));
-});
-
-// Custom 404 handler - This should be the last route
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(echoSitePath, '404.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
